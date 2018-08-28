fa72ab567802218cb20964a3ff1f22ef4b74507f
Aaron Miller <millerac333@gmail.com>
2018-08-28 09:01:47 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Define a List, named goodSongs, that will hold tuples consisting of two strings.
+
+// Define a HashSet, named allSongs, that contains 7 tuples. Each tuple should contain two string values:
+
+// The name of an artist
+// A song by that artist
+
+// // Example
+// HashSet<(string, string)> songs = new HashSet<(string, string)>();
+
+// Make sure that some of the songs are from the band Nickelback. You can see a list of their greatest hits on Amazon.
+
+// Once the set is populated with 7 tuples, iterate over the set of songs, and check if the band name is "Nickelback".
+
+// If the band is not Nickelback, then add it to the goodSongs list.
+
+// USe another foreach loop to print out all the good songs.
+
+using System;
+
+namespace nickelback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
